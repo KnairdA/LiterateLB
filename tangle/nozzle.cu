73fdd53ba37a09092d2bee9de03707aeb3cd6b2e
Adrian Kummerlaender <adrian@kummerlaender.eu>
2021-06-16 22:36:57 +0200

    Extend nozzle channel to prevent outflow divergence

--- tangle/nozzle.cu
+++ tangle/nozzle.cu
@@ -17,7 +17,7 @@ using DESCRIPTOR = descriptor::D3Q19;
 int main() {
 cudaSetDevice(0);
 
-const descriptor::Cuboid<DESCRIPTOR> cuboid(400, 80, 80);
+const descriptor::Cuboid<DESCRIPTOR> cuboid(500, 80, 80);
 Lattice<DESCRIPTOR,T> lattice(cuboid);
 
 CellMaterials<DESCRIPTOR> materials(cuboid, [&cuboid](uint3 p) -> int {
